e52a9764e0d9f34d1a60d8005f1a27e352687c33
Justin Park <justin.park@airbnb.com>
2018-09-12 00:14:21 -0700

    구매하기 창 수정

--- demo-app/src/BuyCoinContent.jsx
+++ demo-app/src/BuyCoinContent.jsx
@@ -10,6 +10,7 @@ class BuyCoinContent extends PureComponent {
   constructor(props) {
     super(props);
     this.handlePriceChange = this.handlePriceChange.bind(this);
+    this.handleStockChange = this.handleStockChange.bind(this);
     this.state = {
       price: props.price,
     };
@@ -21,24 +22,33 @@ class BuyCoinContent extends PureComponent {
     }
   }
 
+  handleStockChange(newStock) {
+    this.setState({ stock: newStock });
+  }
+
   handlePriceChange(newPrice) {
     this.setState({ price: newPrice });
   }
 
   render() {
     const { id, name, updatePrice, ...otherAttrs } = this.props;
-    const { price } = this.state;
+    const { price, stock } = this.state;
+    const total = Number(price) * Number(stock);
     return (
       <div>
         <h1>
-          가격 수정 하기
+          구매하기 하기
         </h1>
         <h4>구매하려는 코인: {name}</h4>
         <h6></h6>
         <Text noLoading>가격</Text>
         <Input value={price} onChange={this.handlePriceChange}/>
-        총 구매 비용: <Text noLoading>얼마</Text>
-        <Button onClick={() => updatePrice(id, Number(price))}>수정</Button>
+        <Text noLoading>총 수량</Text>
+        <Input value={stock} onChange={this.handleStockChange}/>
+        총 구매 비용: <Text noLoading>{total}</Text>
+        <Button onClick={() => updatePrice(id, Number(price))}>
+          구매
+        </Button>
       </div>
     );
   }
